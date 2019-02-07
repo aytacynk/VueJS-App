4dd22e4b7d0a55f901dddcf5c3d9756fb9d2e5a1
Aytach <aytacyanik@hotmail.com.tr>
2019-02-08 01:36:55 +0300

    v-html, v-if, v-show

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ window.addEventListener('load', () => {
     new Vue({
         el: '#app',
         data: {
-            message: 'Hello World!'
+            message: 'Hello World!',
+           // message: '<h1>Hello World!</h1>',
+            isLoggedIn: true,
+            username: 'Aytach'
         }
     });
 });
